1f900f498e3e7661099115316398f185c25660cc
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-21 20:59:27 -0300

    funcao pra incrementar numeros no array

--- escadinhas.py
+++ escadinhas.py
@@ -1,9 +1,37 @@
 #!/usr/local/bin/python
 
+def incrementa(num, base):
+    max = True
+    for p in num:
+        if p != base - 1:
+            max = False
+            break
 
-def escadinha(base):
-    pass
+    if max:
+        num[0] = 1
+        for i in range(1, len(num)):
+            num[i] = 0
+        num.append(0)
+        return num
+
+    if num[-1] < base - 1:
+        num[-1] += 1
+    else:
+        num[-1] = 0
+        for i in range(len(num) - 2, -1, -1):
+            if num[i] < base - 1:
+                num[i] += 1
+                break
+            else:
+                num[i] = 0
+    return num
+
+#def escadinha(base):
+    #count = 0
+    #num = []
+    #for i in range(1, )
 
 
 if __name__ == "__main__":
-    print "oi"
+    num = [4, 5, 5]
+    print incrementa(num, 6)
