ffcad19f0d15a3ca3c26b29f2a730c335c433181
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-21 21:34:41 -0300

    Algoritmo burro funcionando

--- escadinhas.py
+++ escadinhas.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python
 
+import sys
+
 def incrementa(num, base):
     max = True
     for p in num:
@@ -26,12 +28,56 @@ def incrementa(num, base):
                 num[i] = 0
     return num
 
-#def escadinha(base):
-    #count = 0
-    #num = []
-    #for i in range(1, )
+def baseN_to_base_10(num, base):
+    final = 0
+
+    num.reverse()
+    for idx, val in enumerate(num):
+        final += val * pow(idx, base)
+
+    return final
+
+def unique(num):
+    for idx, val in enumerate(num):
+        for nidx, nval in enumerate(num):
+            if val == nval:
+                if idx == nidx:
+                    continue
+                #print "INVALIDO (unique) %s" % num
+                return False
+    return True
+
+def valido(num, base):
+    for idx, val in enumerate(num):
+        if val >= base:
+            #print "INVALIDO (base) %s" % num
+            return False
+        if abs(val - num[idx - 1]) > 2:
+            #print "INVALIDO (modulus 2) %s" % num
+            return False
+
+        return unique(num)
+
+def escadinha(base):
+    count = 0
+    num = [0]
+    maxnum = []
+    for i in range(0, base):
+        maxnum.append(base - 1)
+
+    while num != maxnum:
+        num = incrementa(num, base)
+        #print "TESTANDO %d", num
+        if valido(num, base):
+            count += 1
+            #print "VALIDO %s" % num
+
+    return count
 
 
 if __name__ == "__main__":
-    num = [4, 5, 5]
-    print incrementa(num, 6)
+    if len(sys.argv) < 2:
+        base = 5
+    else:
+        base = int(sys.argv[1])
+    print escadinha(base)
