73f1cbbf9e07679c4725dc870dd7cc78fa03f67e
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-22 14:46:59 -0300

    screw lyx

---
+++ docs/auto/relatorio.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "relatorio"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "brazil")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "latin9") ("babel" "brazil")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fontenc"
+    "inputenc"
+    "algorithm2e"
+    "sbc-template"
+    "babel"
+    "listings")
+   (LaTeX-add-environments
+    '("lyxlist" 1))))
+
