5ad1f5c641700958ff121c1b4cec1fd32bcce497
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-29 17:42:12 -0300

    AGORA TAH CERTO

--- escadinhas4.py
+++ escadinhas4.py
@@ -2,30 +2,30 @@
 import sys
 
 
-def esc(base, num, d, usad):
+def esc(base, num, d, usad, nivel):
     count = 0
-    if len(num) > base:
-        return 1
+    if len(num) >= base:
+        return 0
     if num == [] or (abs(num[-1] - d) <= 2 and not usad[d]):
-        print num
         num.append(d)
+        print num
         usad[d] = True
         count += 1
         for i in range(0, base):
-            count += esc(base, num, i, usad)
+            count += esc(base, num[:], i, usad[:], nivel + 1)
     return count
 
 
 def escadinha(base):
     count = 0
     for i in range(1, base):
-        count += esc(base, [], i, [False for x in range(0, base)])
+        count += esc(base, [], i, [False for x in range(0, base)], 0)
 
     return count
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        base = 5
+        base = 4
     else:
         base = int(sys.argv[1])
     print escadinha(base)
